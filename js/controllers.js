60b8ac8ca6208d77ecc32c8ab2ee9da038a80317
Jasvir005 <jasvir.softweaver@gmail.com>
2016-06-20 00:08:28 -0700

    mon6

--- js/controllers.js
+++ js/controllers.js
@@ -2,17 +2,19 @@ angular.module('starter.controllers', ["ngStorage"])
 
 .controller('AppCtrl', function($ionicHistory,$document,$state,$scope, $ionicModal, $timeout,$http, $sessionStorage,$window,searchdate,$rootScope,$ionicLoading) {
 
+    $timeout(function() {
+$(".div-coloor").hide();
+
+
+         
+          }, 1);
+
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-alert('hello');
-$("#pageload").css("display","none");
-$("#pageload").hide();
-$(".div-coloor").css("display","none important");
-//$(".div-coloor").hide();
 
 
   // Form data for the login modal
