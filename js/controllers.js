10537ff5cdad2b8fad2176c47efdb36406b66727
Jasvir005 <jasvir.softweaver@gmail.com>
2016-06-22 04:42:56 -0700

    thurs26

--- js/controllers.js
+++ js/controllers.js
@@ -8,20 +8,7 @@ angular.module('starter.controllers', ["ngStorage"])
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
   //});
-//alert('hello');
 
- /*  $timeout(function() {
-          //alert('hello');
- $(".main-containerlog").css({"position":"relative","top":"1000px"})
-          }, -1);
-
-   $timeout(function() {
- $(".main-containerlog").css({"position":"relative","top":"0px"})
-
-          }, 3000);*/
-
-
-//$(".div-coloor").hide();
 
 
   // Form data for the login modal
@@ -640,7 +627,7 @@ $(".norating").append('<p class="countrating">1</p>');
 
  
 
-$(".first-star1 img").attr('src',"img/1star copy.png").css({"width":"100%","margin-left":"0px"});
+$(".first-star1 img").attr('src',"img/1star copy.png").css("width","90%");
  
 $(".first-star2 img").attr('src',"img/second_img.png");
 $(".first-star3 img").attr('src',"img/third_img1.png");
@@ -657,14 +644,14 @@ $(".first-star2").click(function(){
 
 $(".norating").append('<p class="countrating">2</p>');
 
-$(".first-star2 img").attr('src',"img/2star copy.png").css({"width":"100%","margin-left":"0px"});
+$(".first-star2 img").attr('src',"img/2star copy.png").css("width","90%");
 
 
 
-$(".first-star1 img").attr('src',"img/first_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star3 img").attr('src',"img/third_img1.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star4 img").attr('src',"img/fourth_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star5 img").attr('src',"img/fifth_img.png").css({"width":"100%","margin-left":"0px"}).css("width","100%");
+$(".first-star1 img").attr('src',"img/first_img.png").css("width","100%");
+$(".first-star3 img").attr('src',"img/third_img1.png").css("width","100%");
+$(".first-star4 img").attr('src',"img/fourth_img.png").css("width","100%");
+$(".first-star5 img").attr('src',"img/fifth_img.png").css("width","100%").css("width","100%");
 
 
 });
@@ -677,12 +664,12 @@ $(".norating").append('<p class="countrating">3</p>');
 
  
 
-$(".first-star3 img").attr('src',"img/3star copy.png").css({"width":"100%","margin-left":"0px"});
+$(".first-star3 img").attr('src',"img/3star copy.png").css("width","90%");
 
-$(".first-star1 img").attr('src',"img/first_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star2 img").attr('src',"img/second_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star4 img").attr('src',"img/fourth_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star5 img").attr('src',"img/fifth_img.png").css({"width":"100%","margin-left":"0px"});
+$(".first-star1 img").attr('src',"img/first_img.png").css("width","100%");
+$(".first-star2 img").attr('src',"img/second_img.png").css("width","100%");
+$(".first-star4 img").attr('src',"img/fourth_img.png").css("width","100%");
+$(".first-star5 img").attr('src',"img/fifth_img.png").css("width","100%");
 
 
 });
@@ -694,13 +681,12 @@ $(".first-star4").click(function(){
 $(".norating").append('<p class="countrating">4</p>');
 
 
+$(".first-star4 img").attr('src',"img/4star copy.png").css("width","90%");
 
-$(".first-star4 img").attr('src',"img/4star copy.png").css({"width":"100%","margin-left":"0px"});
-
-$(".first-star1 img").attr('src',"img/first_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star2 img").attr('src',"img/second_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star3 img").attr('src',"img/third_img1.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star5 img").attr('src',"img/fifth_img.png").css({"width":"100%","margin-left":"0px"});
+$(".first-star1 img").attr('src',"img/first_img.png").css("width","100%");
+$(".first-star2 img").attr('src',"img/second_img.png").css("width","100%");
+$(".first-star3 img").attr('src',"img/third_img1.png").css("width","100%");
+$(".first-star5 img").attr('src',"img/fifth_img.png").css("width","100%");
 
 });
 
@@ -710,12 +696,12 @@ $(".first-star5").click(function(){
     $(".norating").html("");
 $(".norating").append('<p class="countrating">5</p>');
 
- $(".first-star5 img").attr('src',"img/5star.png").css({"width":"100%","margin-left":"0px"});
+ $(".first-star5 img").attr('src',"img/5star.png").css("width","90%");
 
-$(".first-star1 img").attr('src',"img/first_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star2 img").attr('src',"img/second_img.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star3 img").attr('src',"img/third_img1.png").css({"width":"100%","margin-left":"0px"});
-$(".first-star4 img").attr('src',"img/fourth_img.png").css({"width":"100%","margin-left":"0px"});
+$(".first-star1 img").attr('src',"img/first_img.png").css("width","100%");
+$(".first-star2 img").attr('src',"img/second_img.png").css("width","100%");
+$(".first-star3 img").attr('src',"img/third_img1.png").css("width","100%");
+$(".first-star4 img").attr('src',"img/fourth_img.png").css("width","100%");
  
 });
  
@@ -750,9 +736,7 @@ $('.right-side-categories').click(function(){
 
 var clicks = $(".right-side-categories").data('clicks');
 if (clicks) {
-//alert('truedd');
- $(".filter-page-contant .right-side-categories img").removeClass("rtansformimga");
-
+//alert('true');
 
 var ua = navigator.userAgent.toLowerCase();
 var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
@@ -789,7 +773,10 @@ else
        var widthvar = $( window ).width();
        //alert('hi');
        //alert(widthvar);
+
  
+ 
+
        if(widthvar==360)
        {
 
@@ -870,13 +857,9 @@ $(".categories-div-left").css({"left":"0px","padding":"1px","width":"201px","top
 $(".padding").css({"padding": "0px"});
 
 }
- 
-//alert('false hi ');
+///alert('false ');
  
 
-  $(".filter-page-contant .right-side-categories img").addClass("rtansformimga");
-
- 
 }
 
  
@@ -1190,7 +1173,7 @@ var text = '{ "usertype":"2" , "email":"jasvir.softweaver@gmail.com" ,"password"
  
   $scope.doSignud = function() {
 
-    //alert('hello');
+    alert('hello');
   /*    $ionicLoading.show({
         template: '<img  src="img/loading.gif" />'
       });
