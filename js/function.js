dbbcb304783ba3a867a7aff4f14419b1a914d966
Jasvir005 <jasvir.softweaver@gmail.com>
2016-07-16 07:17:24 -0700

    jmd1

--- js/function.js
+++ js/function.js
@@ -23,6 +23,18 @@ var starimage="http://gotaworkout.com/service/public/z_uploads/doctor/star_image
 	var speciality=val.speciality;
 		var usrids=val.usrid;
 
+
+var str = val.firstname;
+str = str.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+    return letter.toUpperCase();
+});
+//alert(str); //Displays "Hello World"
+
+
+
+
+
+
 if(val.userimage==null)
 {
 var image="<img src="+noimage+">";
@@ -39,7 +51,7 @@ var image="<img src="+src+val.userimage+">";
 
 
 
-		data="<div id='searchlop'  onClick='abd("+val.usrid+")' class='discover-content"+speciality+"'><div class='content-part'>"+image+"<div class='top-of-content'></div><div class='middel-part'><div class='left-part-middel'><div class='text-part-left'><h1>"+val.firstname+"</h1><p class='categories-append'></p><div class='countratinga'><div class='rating' id='rating"+val.usrid+"'></div><div id='round"+val.usrid+"' class='round-count'></div></div></div></div><div class='text-right-part'><h1>$"+val.payrate+" / Hour</h1></div></div></div></div>";
+		data="<div id='searchlop'  onClick='abd("+val.usrid+")' class='discover-content"+speciality+"'><div class='content-part'>"+image+"<div class='top-of-content'></div><div class='middel-part'><div class='left-part-middel'><div class='text-part-left'><h1>"+str+"</h1><p class='categories-append'></p><div class='countratinga'><div class='rating' id='rating"+val.usrid+"'></div><div id='round"+val.usrid+"' class='round-count'></div></div></div></div><div class='text-right-part'><h1>$"+val.payrate+" / Hour</h1></div></div></div></div><div class='spacediv'></div>";
 
 		jQuery(".discover-content").append(data);
 
@@ -136,14 +148,6 @@ $("#round"+useridfor+"").append(ratinga);
 }
 
  
-
-
- 
-
-
- 
- 
-
 //alert(ratinga);
 //console.log(ratinga);
 //alert(usrids);
@@ -180,6 +184,183 @@ $(".discover-content"+speciality+" .text-part-left"+val.id+" p").append("");
 } // function close
 
 
+
+
+
+
+
+
+/*    function home page click */
+
+
+$(document).ready(function()
+{
+
+$(".page_filter").click(function()
+{
+$(".first-page-all-trainer").hide();
+$(".loginhome-page").show();
+});
+
+/*login page open */
+$("#login-world").click(function()
+{
+$(".loginhome-page").hide();
+$(".signup-page-div").hide();
+$(".login-page").show();
+});
+
+/*  signup page open */
+$("#signdiv").click(function(){
+$(".loginhome-page").hide();
+$(".signup-page-div").show();
+});
+
+ /*login page close  and signup page close  */
+
+
+$(".login-backbtn").click(function()
+{
+$(".login-page").hide();	
+$(".signup-page-div").hide();
+$(".loginhome-page").show();
+});
+
+ 
+
+
+
+ $(".signup-btn").click(function(){
+
+
+var checkedis=$("#checkbox-terms").is(':checked');
+
+var emailAddressfd=$("#seremail-validate").val();
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+if(emailAddressfd=="")
+{
+
+$("#seremail-validate").attr("placeholder", "E-Mail is required");
+
+} 
+else if(!emailAddressfd.match(re))
+{
+
+$("#seremail-validate").val("");
+$("#seremail-validate").attr("placeholder", "Invalid E-Mail");
+return false;
+ 
+ }
+
+ else if(checkedis=="")
+{
+$("#must-agree").show();
+}
+
+
+/*  checkbox message  */
+ else
+ {
+
+
+
+var name= $("#fname").val();
+var lname= $("#lname").val();
+var email= $("#seremail-validate").val();
+
+
+var text = '{ "firstname":"'+name+'" , "lastName":"'+lname+'" ,"email":"'+email+'","usertype":"2" }';
+ 
+ 
+jQuery.ajax({
+url: "http://gotaworkout.com/index.php/get_signup_details2",
+method: "GET",
+data: {'signUpData': text},
+
+success: function(alluserss) {
+ 
+$("#resultappen").show();
+
+var obj = jQuery.parseJSON( alluserss );
+var msg= obj.msg;
+$("#resultappen").html(msg);
+//alert( obj.msg );
+
+}
+
+});
+
+ } /*  ajax request message  */
+
+}); //signup click request
+
+
+/* *********************login request************************/
+
+//login_id
+
+$("#login_id").click(function(){
+var useremail= $("#login_user_id").val();
+var userid= $("#user_paswd").val();
+
+var useremail=$("#login_user_id").val();
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+
+if(useremail=="")
+{
+
+$("#login_user_id").attr("placeholder", "E-Mail is required");
+
+} 
+else if(!useremail.match(re))
+{
+
+$("#login_user_id").val("");
+$("#login_user_id").attr("placeholder", "Invalid E-Mail");
+return false;
+ 
+ }
+ else
+ {
+
+var emailId= $("#login_user_id").val();
+var passNme= $("#user_paswd").val();
+
+
+var text2 = '{ "email":"'+emailId+'" , "password":"'+passNme+'" ,"usertype":"2"}';
+ 
+ 
+jQuery.ajax({
+url: "http://gotaworkout.com/index.php/get_login_details",
+method: "GET",
+data: {'loginData': text2},
+
+success: function(loginuser) {
+var obj = jQuery.parseJSON( loginuser );
+var userID= obj.userID;
+if(userID==null)
+{
+$("#login_error").show();
+}
+
+}
+
+});
+
+}
+
+}); //login request end
+
+
+
+
+
+
+
+
+})
 
 
 
