f2e36fae6310d7d5e099801b91bee3ef49810858
Jasvir005 <jasvir.softweaver@gmail.com>
2016-07-22 19:55:22 +0530

    jmd64

--- js/function.js
+++ js/function.js
@@ -588,7 +588,7 @@ fbLoginSuccess = function (userData) {
 
 
 fbData = function () {
-
+ jQuery("body").addClass("show1");
  // alert("fbdata function");
   //alert('hello');
     facebookConnectPlugin.api( "me/?fields=id,name,email", ["email"],
@@ -616,7 +616,7 @@ method: "GET",
 data: {'signUpData': text},
 
 success: function(alluserss) {
- jQuery("body").addClass("show1");
+  jQuery("body").addClass("show1");
 
 var obj = jQuery.parseJSON( alluserss );
 //alert(obj.status);
@@ -638,7 +638,7 @@ success: function(alluserssd) {
 var userID= objs.userID;
 var review=localStorage.getItem('review');
 
-//alert(review);
+alert(review);
 if(review=="reviewlogin")
 {
 
