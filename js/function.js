3091994689712dcc3a7d6b80d5ef1d1009e51d34
Jasvir005 <jasvir.softweaver@gmail.com>
2016-08-04 13:43:17 +0530

    booking49

--- js/function.js
+++ js/function.js
@@ -172,7 +172,7 @@ else
 {
 
 function onConfirm(buttonIndex) {
-    alert('You selected button ' + buttonIndex);
+   // alert('You selected button ' + buttonIndex);
 }
 
 navigator.notification.confirm(
